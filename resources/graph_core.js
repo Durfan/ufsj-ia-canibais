891a0936b7603aa5f379f5e08073e7ebda16d35b
Durfan <31577607+Durfan@users.noreply.github.com>
2019-09-24 01:22:01 -0300

    front upt

--- resources/graph_core.js
+++ resources/graph_core.js
@@ -1,6 +1,9 @@
-const Graph = ForceGraph()
-	(document.getElementById('graph'))
-		.graphData(gData)
-		.nodeRelSize(10)
-		.linkDirectionalParticles(1)
-		.linkDirectionalArrowLength(6);
+const elem = document.getElementById('graph');
+
+const Graph = ForceGraph()(elem)
+	.graphData(gData)
+	.nodeRelSize(9)
+	.linkWidth(5)
+	.linkDirectionalParticles(1)
+	.linkDirectionalArrowLength(6)
+	.onNodeHover(node => elem.style.cursor = node ? 'pointer' : null);
