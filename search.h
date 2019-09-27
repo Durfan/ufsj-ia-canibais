0f64703bd376610ab93c583dead9983246c4c7f1
Durfan <31577607+Durfan@users.noreply.github.com>
2019-09-27 01:13:40 -0300

    lib del in root

--- search.h
+++
@@ -1,5 +0,0 @@
-
-
-int **depthSearch(State start, State *hashmap);
-int expandAndStack(State state, State *hashmap, Stack *stack);
-void constructionSolution(State start, int actualNode, State *hashmap, int **graph);
