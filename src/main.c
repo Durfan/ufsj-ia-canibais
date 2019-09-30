59b74e90f2071732b047e2509377f006ae1d36e3
Durfan <31577607+Durfan@users.noreply.github.com>
2019-09-30 01:07:48 -0300

    tutorial comments rmv

--- src/main.c
+++ src/main.c
@@ -36,44 +36,29 @@ int main(int argc, char **argv) {
     timeresult(start,"profund");
 
 
-	// Initialize GTK+
     gtk_init(&argc, &argv);
 
-	GtkWidget *mainWindow;
-  	GdkPixbuf *appicon;
+	GtkWidget *mainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  	GdkPixbuf *appicon = create_pixbuf(icon);
 
-	// Create an 800x600 window that will contain the browser instance
-    mainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(mainWindow), "Missionarios vs. Canibais");
 	gtk_window_set_default_size(GTK_WINDOW(mainWindow), 800, 600);
 	gtk_window_set_position(GTK_WINDOW(mainWindow), GTK_WIN_POS_CENTER);
-
-	appicon = create_pixbuf(icon);
   	gtk_window_set_icon(GTK_WINDOW(mainWindow), appicon);
 
-	// Create a browser instance
     WebKitWebView *webView = WEBKIT_WEB_VIEW(webkit_web_view_new());
-
-	// Put the browser area into the main window
     gtk_container_add(GTK_CONTAINER(mainWindow), GTK_WIDGET(webView));
 
-	// Set up callbacks so that if either the main window or the browser instance is
-    // closed, the program will exit
     g_signal_connect(mainWindow, "destroy", G_CALLBACK(destroyWindowCb), NULL);
     g_signal_connect(webView, "close", G_CALLBACK(closeWebViewCb), mainWindow);
 
-	// Load a web page into the browser instance
     webkit_web_view_load_uri(webView, home);
 
-	// Make sure that when the browser area becomes visible, it will get mouse
-    // and keyboard events
     gtk_widget_grab_focus(GTK_WIDGET(webView));
-
-    // Make sure the main window and all its contents are visible
     gtk_widget_show_all(mainWindow);
+
 	g_object_unref(appicon);
 
-    // Run the main GTK+ event loop
     gtk_main();
 
 	return 0;
