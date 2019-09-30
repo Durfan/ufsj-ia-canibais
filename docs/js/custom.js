2b7f553b8c8dccd2b6ffa04d5aca339d5a0d1bc1
Durfan <31577607+Durfan@users.noreply.github.com>
2019-09-30 02:10:39 -0300

    interface updt

---
+++ docs/js/custom.js
@@ -0,0 +1,3 @@
+document.getElementById('canibal').draggable = false;
+document.getElementById('cleric').draggable = false;
+document.getElementById('logo').draggable = false;
--- resources/graphs/profund.js
+++ resources/graphs/profund.js
@@ -56,4 +56,4 @@ const gData = {
 
 $('span.memoria').text('25');
 
-$('span.tempo').text('0.000482s');
+$('span.tempo').text('0.000664s');
